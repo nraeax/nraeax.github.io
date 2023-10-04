1f0e67c9fb3f86e783e61674dcf02de3759d5a20
Nat <nraeax@gmail.com>
2023-10-04 02:24:02 +0100

    Initial commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,14 @@
+export default {
+  title: 'nraeax/dev',
+  description: 'Vite & Vue powered static site generator.',
+  head: [
+  	['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+  themeConfig: {
+    nav: [
+      { text: 'Articles', link: '/articles/' },
+      { text: 'Projects', link: '/projects/' },
+      { text: 'Contact', link: '/contact/' }
+    ],
+  }
+}
---
+++ docs/articles/index.md
@@ -0,0 +1,3 @@
+# Articles Index
+
+[Article 1](/articles/article-1)
