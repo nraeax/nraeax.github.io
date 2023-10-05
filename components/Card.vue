04ecdf495be32d7daeea4ecc67d3b7f93fa69bbf
Nat <nraeax@gmail.com>
2023-10-05 20:32:10 +0100

    Update card component and add images

--- components/Card.vue
+++ components/Card.vue
@@ -1,20 +1,25 @@
 <script setup lang="ts">
+
   const props = defineProps({
     href: String,
     title: String,
     date: String,
-    image: {
-      type: Object,
-      required: true,
-    }
-
+    src: String,
+    alt: String
   })
+
 </script>
 
 <template>
-  <a :href="href">
-    <img :src="image" :alt="image">
-    <h2>{{ title}}</h2>
-    <time :datetime="date">{{ date }}</time>
-  </a>
+  <div class="w-full flex flex-col overflow-hidden rounded-lg ring-2 ring-slate-200 dark:ring-white/25">
+    <a :href="href">
+      <div>
+        <img :src="src" :alt="alt">
+      </div>
+      <div class="p-3">
+        <h3 class="text-lg font-bold">{{ title }}</h3>
+        <time :datetime="date">{{ date }}</time>
+      </div>
+    </a>
+  </div>
 </template>
