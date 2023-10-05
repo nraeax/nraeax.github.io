e704eff7b6b4dbf9c7342aad3cfba42081d52aaa
Nat <nraeax@gmail.com>
2023-10-05 08:13:28 +0100

    Update card component and article meta data

--- components/Card.vue
+++ components/Card.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
-  defineProps({
+  const props = defineProps({
     href: String,
     title: String,
     date: String,
+    image: {
+      type: Object,
+      required: true,
+    }
+
   })
 </script>
 
 <template>
   <a :href="href">
+    <img :src="image" :alt="image">
     <h2>{{ title}}</h2>
     <time :datetime="date">{{ date }}</time>
   </a>
--- content/articles/article-2.md
+++ content/articles/article-2.md
@@ -5,9 +5,9 @@ summary:  An introductory blog post
 author:
   name: Natalie Andrews
   github: nraeax
-featureImage:
+featuredImage:
   url: /images/featured-image.jpg
-  title: Featured image
+  title: hello world
 publishDate: 04 March 2023
 publishDateTime: 2023-03-04:17:30
 ---
