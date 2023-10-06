6a3d8b4216a196ed9c66471103dff52d41214bfb
Nat <nraeax@gmail.com>
2023-10-06 01:13:18 +0100

    Update Categories.vue

--- components/Categories.vue
+++ components/Categories.vue
@@ -7,9 +7,15 @@
 
 <template>
 	<aside class="col-span-3">
-		<h2>Categories</h2>
+		<h2 class="mb-6">Categories</h2>
 		<ul>
-			<li v-for="(category, index) in allCategories" :key="index">{{ category }}</li>
+			<li 
+				v-for="(category, index) in allCategories" 
+				:key="index"
+				class="py-1"
+			>
+					{{ category }}
+			</li>
 		</ul>
 	</aside>
 </template>
