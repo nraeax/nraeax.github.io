1940d4073b127e3088c4b90c8ad639d2eebc86d7
Nat <nraeax@gmail.com>
2023-10-08 00:11:59 +0100

    Add mobile menu button

--- components/HeaderNavigation.vue
+++ components/HeaderNavigation.vue
@@ -1,8 +1,9 @@
 <template>
-	<nav class="flex items-center justify-between gap-6">
+	<nav class="hidden md:flex items-center justify-between gap-6">
 		<NuxtLink to="/" class="text-sm" activeClass="text-blue-500">Home</NuxtLink>
 		<NuxtLink to="/articles" class="text-sm font-medium" activeClass="text-blue-500">Articles</NuxtLink>
 		<NuxtLink to="/projects" class="text-sm font-medium" activeClass="text-blue-500">Projects</NuxtLink>
 		<NuxtLink to="/contact" class="text-sm font-medium" activeClass="text-blue-500">Contact</NuxtLink>
 	</nav>
+	<button class="w-6 h-6 flex items-center justify-center md:hidden"><Icon name="heroicons:bars-3-20-solid" class="w-5 h-5"/></button>
 </template>
