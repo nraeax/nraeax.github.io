03e5290010a8865aafafec66ff2cb9a0a77a5564
Nat <nraeax@gmail.com>
2023-10-09 00:26:44 +0100

    Add buttons and styles

--- components/Hero.vue
+++ components/Hero.vue
@@ -4,6 +4,14 @@
 			<h1 class="text-6xl text-green-500">Lorem ipsum</h1>
 			<h2 class="text-6xl">Take control of your day and conquer life.</h2>
 			<p class="text-xl">Reduce stress, free up time and focus on the things that you love.</p>
+			<div class="flex gap-6">
+				<button class="button button--primary">
+					Button 1
+				</button>
+				<button class="button button--outline">
+					Button 2
+				</button>
+			</div>
 		</div>
 	</div>
 </template>
